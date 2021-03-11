309bd591e1529e55a0008afc518dc5ba5f2daf7d
Ricardo Delfin <me@rdelfin.com>
2021-03-11 21:26:15 +0000

    Updated schema.rs

--- api/src/schema.rs
+++ api/src/schema.rs
@@ -5,3 +5,27 @@ table! {
         author -> Text,
     }
 }
+
+table! {
+    owned_books (isbn, user_id) {
+        isbn -> Text,
+        user_id -> Integer,
+    }
+}
+
+table! {
+    user (id) {
+        id -> Nullable<Integer>,
+        username -> Text,
+        password -> Text,
+    }
+}
+
+joinable!(owned_books -> book (isbn));
+joinable!(owned_books -> user (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    book,
+    owned_books,
+    user,
+);
