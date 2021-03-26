c65e0b45155b80a1a73074fb58b23914c40923fa
Ricardo Delfin <me@rdelfin.com>
2021-03-26 22:32:33 +0000

    Removed unused imports

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,8 @@
  * @format
  */
 
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
-import { Appbar } from "react-native-paper";
 import Home from "./pages/Home";
 
 export default function App() {
