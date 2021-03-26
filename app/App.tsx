e6f6042aef55488a256bb315617031e0073d0420
Ricardo Delfin <me@rdelfin.com>
2021-03-26 22:32:01 +0000

    Created pages subfolder

--- app/App.tsx
+++ app/App.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { Appbar } from "react-native-paper";
-import Home from "./Home";
+import Home from "./pages/Home";
 
 export default function App() {
     return (
